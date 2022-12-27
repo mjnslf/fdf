3381d8111fa1a3c99b9d21c4b603b51e0084ee68
mjnslf <67310860+mjnslf@users.noreply.github.com>
2022-12-27 10:57:43 +0600

    bomb

---
+++ cleanDisk.sh
@@ -0,0 +1,12 @@
+sudo rm -rf ~/Library/Caches
+sudo rm -rf ~/Library/Application\ Support/Code/Cache
+sudo rm -rf ~/Library/Application\ Support/Code/CachedData
+sudo rm -rf ~/Library/Application\ Support/Code/CachedExtension
+sudo rm -rf ~/Library/Application\ Support/Code/CachedExtensions
+sudo rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs
+sudo rm -rf ~/Library/Application\ Support/Code/Code\ Cache
+sudo rm -rf ~/Library/Application\ Support/Slack/Cache
+sudo rm -rf ~/Library/Application\ Support/Slack/Code\ Cache
+sudo rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+sudo rm -rf ~/Library/Application\ Support/Spotify/PersistentCache
+
